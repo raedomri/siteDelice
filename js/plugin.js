da2e41e8a923b0e74b444cfbfeaf4dbbff6556c9
raedomri <raed.omri05@gmail.com>
2022-05-20 22:34:39 +0100

    first commit

---
+++ js/plugin.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+ $(".box").click(function(){
+   $(this).css("height","900px")
+   
+   
+   
+})
+$(".box").mouseleave(function(){
+    $(this).css("height","370px")
+})
+
+});
+
+$(document).ready(function(){
+
+    $(".boxe").click(function(){
+      $(this).css("height","800px")
+      
+      
+      
+   })
+   $(".boxe").mouseleave(function(){
+       $(this).css("height","370px")
+   })
+   
+   });
+
+  /* $(function(){
+       "use strict";
+       $(".box").bind("click", function(){
+           $(this).hide();
+       });
+
+   });*/
